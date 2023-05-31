5723235ff99eeb857a72ddc17f1d44d594c82547
Nat <nat.ja@mail.ru>
2023-05-31 22:26:37 +0500

    add model Book

--- djangoTry1/app1/models.py
+++ djangoTry1/app1/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(
+        max_length=64,
+        verbose_name='Название книги'
+    )
+    author = models.CharField(
+        max_length=32,
+        verbose_name='Автор'
+    )
+    publication_year = models.IntegerField(
+        verbose_name='Год издательства'
+    )
+    price = models.FloatField(
+        verbose_name='Цена'
+    )
