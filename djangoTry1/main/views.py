b185544e72bd593bfbc13e7530102871ffe83a83
Nat66615 <134211493+Nat66615@users.noreply.github.com>
2023-06-01 09:37:19 +0500

    Update views.py

--- djangoTry1/main/views.py
+++ djangoTry1/main/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Привет, моё солнышко!)</h1>")
+    return HttpResponse("<h1>Привет, мир!</h1>")
 
